34aeb007bfe51a3a0117b798910ba0f55ea76b29
Liz Schley <lizschley@gmail.com>
2015-04-05 14:25:49 -0400

    finishing file io assignment

--- io/file_using.rb
+++ io/file_using.rb
@@ -1,18 +1,39 @@
 class FileUsing
-    def initialize(file)
-        @file = file
+    require "date"
+    def initialize(in_file)
+        @file = File.new(in_file, "a+")
+        @filename = in_file
     end
     def in_out
-        @file.readlines.each do |line|
-            puts line
+        if !@file.instance_of? File
+            puts 'run FileUsing.new(filename)'
+            return
         end
-        @file.write 'this file has been seen - this line should be last'        
+        @file.readlines.each do |line|
+             puts line
+        end
+        @file.puts "this file has been seen on #{get_datetime}" 
+        @file.rewind  
+    end
+
+    def get_datetime
+        current = DateTime.now
+        out_string = current.month.to_s + "-" + current.day.to_s + "-" + current.year.to_s + 
+        " @ " + current.hour.to_s + ":" + current.minute.to_s + ":" + current.second.to_s
+    end
+
+    def append_line_to_beginning
         @file.rewind
-        @file.write 'this file has been seen - this line should be first'
+        File.open("#{@filename}.new", 'w') do |f2|  
+          # use "\n" for two lines of text  
+          f2.puts "added new first line on #{get_datetime}" 
+          @file.readlines.each do |line|
+            f2.puts line
+          end 
+        end
+        @file.close
+        @file = File.new("#{@filename}.new", "a+")
     end
 
 end
 
-# Make this work with so you can make the beginning and end work over and over
-# have initialize take filname and have file
-# start learn ruby the hard way and figure out where to start
